183ea96ae18dac502415fd6a6b760420d95ae427
DeterminedPanda <34836838+determinedpanda@users.noreply.github.com>
2025-01-19 01:04:38 +0100

    added "elapsed" time option for the timestamp

--- DifficultBulletinBoardDefaults.lua
+++ DifficultBulletinBoardDefaults.lua
@@ -4,6 +4,8 @@ DifficultBulletinBoardDefaults.version = 8
 
 DifficultBulletinBoardDefaults.defaultFontSize = 14
 
+DifficultBulletinBoardDefaults.defaultTimeFormat = "fixed"
+
 DifficultBulletinBoardDefaults.defaultNumberOfGroupPlaceholders = 3
 
 DifficultBulletinBoardDefaults.defaultGroupTopics = {
